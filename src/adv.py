3978fed109468b10c07a4a3d59556ab5057ed2d6
Josiah <reikiryo@yahoo.com>
2020-06-04 12:39:15 -0400

    MVP, functional game

--- src/adv.py
+++ src/adv.py
@@ -1,9 +1,10 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", """North of you, the cave mount beckons."""),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -15,7 +16,7 @@ the distance, but there is no way across the chasm."""),
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasury': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -29,8 +30,8 @@ room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['narrow'].n_to = room['treasury']
+room['treasury'].s_to = room['narrow']
 
 #
 # Main
@@ -48,3 +49,27 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+def game():
+    print("\nYou've come from far away seeking to gain treasure in this dungeon.")
+    name = input("\nWhat is your name? ")
+    player = Player(name, room['outside'])
+    print(
+        f"\nWelcome {name}.\nAfter a long nights rest you feel ready to enter the dungeon.\n{player.room.desc}")
+    print("\nControls::\n 'n' to go North.\n 's' to go South.\n 'e' to go East.\n 'w' to go West.\n 'q' to Quit.")
+    player_input = ''
+    while player_input != 'q':
+        player_input = input("\nWhich direction do you head? ")
+        new_room = player.room.check_room(player_input)
+        if new_room == 'Quit':
+            print("\nGame has ended.")
+        elif new_room != None:
+            player.room = new_room
+            print(f"\nYou enter the {player.room.name}")
+            print(f"\n{player.room.desc}")
+        else:
+            print("\nThere is no passage this way.\n You walk into a wall.")
+
+
+game()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,49 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+
+    def check_room(self, dire):
+        if dire == 'n':
+            if hasattr(self, 'n_to'):
+                return self.n_to
+        elif dire == 's':
+            if hasattr(self, 's_to'):
+                return self.s_to
+        elif dire == 'e':
+            if hasattr(self, 'e_to'):
+                return self.e_to
+        elif dire == 'w':
+            if hasattr(self, 'w_to'):
+                return self.w_to
+        elif dire == 'q':
+            return 'Quit'
+        else:
+            return None
+
+            # def north(self):
+            #     if self.n_to:
+            #         return self.n_to
+            #     else:
+            #         return False
+
+            # def south(self):
+            #     if self.s_to:
+            #         return self.s_to
+            #     else:
+            #         return False
+
+            # def east(self):
+            #     if self.e_to:
+            #         return self.e_to
+            #     else:
+            #         return False
+
+            # def west(self):
+            #     if self.w_to:
+            #         return self.w_to
+            #     else:
+            #         return False
